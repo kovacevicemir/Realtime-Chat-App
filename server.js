cec94992734d38a630e56f00ac48745d55965c58
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-14 14:09:12 +1000

    establishing connection between client and server socket io

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 //Set static folder
-
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Run when client connects
+io.on('connection', socket => {
+    console.log('new web socket connection...')
+
+    socket.emit('message', 'Welcome to chatCord')
+})
 
 const PORT = 3000 || process.env.PORT
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
